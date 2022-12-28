58a617453fb3a53979395093693c90f6398f1f80
cihat-salik <cihat@jotform.com>
2022-12-28 15:20:39 +0300

    Add learn-cpp first(number 0) chapter tutorials codebase that

---
+++ computer-science/learn-cpp/cpp-tutorial-1/hello-world.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// #include <string>
+
+using namespace std;
+
+int main()
+{
+
+  string str1 = "Hello";
+  string str2 = "World";
+  string str3;
+  string str4 = "Hello World";
+  int value = 234;
+  string test;
+  int value2 = 0;
+  string str5 = "cihatsalik";
+
+  string str6 = "test test 2";
+  // string str = 234;
+
+  string str7 = "test test 2";
+
+  cout << str6 << endl;
+
+  return 0;
+}
