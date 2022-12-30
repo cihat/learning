17de4ada24f341c617d8335d59f122fb8ad77e89
cihat-salik <cihat@jotform.com>
2022-12-30 16:49:40 +0300

    Add examples in cpp-course

---
+++ computer-science/learn-cpp/cpp-course/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "variables.cpp"
+#include "sum.h"
+
+int main()
+{
+
+  // Given two numbers
+  int a = 13, b = 22;
+
+  // Function declared in header
+  // file to find the sum
+  cout << "Sum is: "
+       << sumOfTwoNumbers(a, b)
+       << endl;
+
+  // int value = variables();
+  // std::cout << value << std::endl;
+}
