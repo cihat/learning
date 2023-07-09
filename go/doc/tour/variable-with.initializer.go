a95db6caec2e4d720adf6b5ea8154bdae04231f3
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-07-09 13:41:45 +0300

    Add starting learn go with doc

---
+++ go/doc/tour/variable-with.initializer.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java string = "true", "false", "no!"
+	var test string = "23"
+	fmt.Println(i, j, c, python, java, test)
+}
