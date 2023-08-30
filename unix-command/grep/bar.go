c71dc21daf518c05229d45fb81f06430f6361b2e
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2023-08-30 19:19:06 +0300

    Add unix commands

---
+++ unix-command/grep/bar.go
@@ -0,0 +1,28 @@
+package main
+
+// Bar() is a function that returns someone's feeling ...
+
+func bar() string {
+	return "test"
+}
+// Copyright 2017 Bill. All rights reserved.
+// created by bill on 10 Oct, 2017
+
+package foo
+
+import (
+    "github.com/go-foo/foo/internal/log"
+)
+
+const VERSION = "1.0.1"
+
+// Foo is a name, just for fun.
+type Foo struct {}
+func (f *Foo) Bar() {
+    log.Println("bar!", VERSION)
+}
+
+// NewFoo returns an instance of Foo.
+func NewFoo() *Foo {
+    return &Foo{}
+}
