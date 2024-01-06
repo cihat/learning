472ec24a6b855fa4daed1896b6a26152a0938048
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-01-06 22:11:06 +0300

    Add learn go channels use-cases

---
+++ go/go-channel-usecases/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//"github.com/cihat/go-channel-usecases/receiveonlysquares"
+	"github.com/cihat/go-channel-usecases/sendonlychannels"
+)
+
+func main() {
+	// receiveonlysquares.Run()
+	sendonlychannels.Run()
+}
