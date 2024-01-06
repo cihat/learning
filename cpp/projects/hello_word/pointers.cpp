5884a277a8e6712625da11874955f2fc53c1710b
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-01-06 22:10:46 +0300

    Add some cpp codes

--- cpp/projects/hello_word/pointers.cpp
+++ cpp/projects/hello_word/pointers.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main()
-{
-  // pointers = variable that stores a memory address of another variable sometimes it's easier to work with an address
+int main() {
+  // pointers = variable that stores a memory address of another variable
+  // sometimes it's easier to work with an address
 
   // & address-of operator
   // * dereference operator
